bdc1e87279ba8eef8e5f574b5919d499f2decbe6
李sir <49725071+lsq1998515@users.noreply.github.com>
2019-10-26 23:36:53 +0800

    Create Queue.h

---
+++ BTree/Queue.h
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#pragma once
+#include <stdio.h>
+#include<stdlib.h>
+#include <assert.h>
+#include<malloc.h>
+#include"Btree.h"
+
+typedef BTNode* QUDataType;
+typedef struct QueueNode
+{
+	struct QueueNode* _next;
+	QUDataType _data;
+}QueueNode;
+typedef struct Queue
+{
+	QueueNode* _front; // 队头
+	QueueNode* _rear; // 队尾
+}Queue;
+
+//初始化
+void QueueInit(Queue* pq);
+//销毁
+void QueueDestory(Queue* pq);
+//创建新的结点
+QueueNode* BuyQueueNode(QUDataType x);
+//入队
+void QueuePush(Queue* pq, QUDataType x);
+//出队
+void QueuePop(Queue* pq);
+//返回队首
+QUDataType QueueFront(Queue* pq);
+//返回队尾
+QUDataType QueueBack(Queue* pq);
+//判断是否为空
+int QueueEmpty(Queue* pq);
+//返回队元素个数
+int QueueSize(Queue* pq);
+//打印
+void QueueDisplay(Queue*pq);
